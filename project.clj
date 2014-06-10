c2505dbabcfc1bbf276076414c118b5c29efb3eb
elegantmush <elegantmush@gmail.com>
2014-06-10 17:41:05 +0100

    adding the start of incanter integration using example  https://github.com/rururu/compojure-incanter as a reference

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
+                 [incanter "1.5.5"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler log4jeo.handler/app}
   :profiles
---
+++ src/log4jeo/views.clj
@@ -0,0 +1,32 @@
+(ns log4jeo.views
+  (:use [hiccup core page]
+        [incanter core stats charts]
+        [compojure response])
+  (:import (java.io ByteArrayOutputStream
+                    ByteArrayInputStream)))
+
+
+(defn to-in-stream [chart]
+	(let [out-stream (ByteArrayOutputStream.)]
+		(save chart out-stream)
+        (ByteArrayInputStream.
+        	(.toByteArray out-stream))))
+
+
+(defn gen-samp-hist-png [size_s mean_s sd_s]
+(let [size (if (empty? size_s) 1000 (Integer. size_s))
+          m (if (empty? mean_s) 1 (Integer. mean_s))
+          s (if (empty? sd_s) 1 (Integer. sd_s))
+          samp (sample-normal size
+                    :mean m
+                    :sd s)
+          chart (histogram
+                 samp
+                 :title "Normal Sample"
+                 :x-label (str "sample-size = " size
+                               ", mean = " m
+                               ", sd = " s))
+          in-stream (to-in-stream chart)]
+      {:status 200
+       :headers {"Content-Type" "image/png"}
+       :body in-stream}))
