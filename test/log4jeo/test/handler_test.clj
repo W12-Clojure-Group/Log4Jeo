6e8c977bd4410b63a2d10c32faeccf35ddad510c
Rich Lyne <richard@onlyne.co.uk>
2014-07-06 23:49:10 +0100

    lyner: Renaming and fixing handler test. Removed test for default page content.

--- test/log4jeo/test/handler_test.clj
+++ test/log4jeo/test/handler_test.clj
@@ -4,11 +4,16 @@
         log4jeo.handler))
 
 (deftest test-app
+
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
+    )
+  )
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 404))
+    )
+  )
+)
