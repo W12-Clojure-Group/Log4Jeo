9a7b11e480d1914102914e2eb8e1a1928ea68beb
AidyLewis <adrian_lewis@fastmail.fm>
2014-07-16 12:01:45 +0100

    returns ip from apache log

--- src/log4jeo/convert.clj
+++ src/log4jeo/convert.clj
@@ -4,5 +4,22 @@
   (def i (map #(Integer/parseInt %)
              (clojure.string/split ipv4 #"\.")))
   (if (not= 4 (count i)) (throw (NumberFormatException. (str "Expected 4 octets in IPv4 address not " (count i)))))
-  (str "::ffff:" (nth i 0) "." (nth i 1) "." (nth i 2) "." (nth i 3) )
-)
+  (str "::ffff:" (nth i 0) "." (nth i 1) "." (nth i 2) "." (nth i 3)))
+
+
+(def filepath "/Users/lewisa29/Projects/Log4Jeo/test/fixtures/apache_log_head")
+(def log (slurp filepath))
+(def ip-address-reg-ex
+ #"(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])")
+(def return-ip (re-find ip-address-reg-ex log))
+
+
+
+
+
+
+
+
+
+
+
